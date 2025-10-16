886d07cad549b56459cee0d5abefe340e0ce79a8
blinky32123 <jkrepner2028@kehillahstudent.org>
2025-10-16 09:12:39 -0700

    helllo

--- project 1a.py
+++ project 1a.py
@@ -1,43 +1,32 @@
 import turtle
+turtle.speed(10)
 
-def draw_square( num_sides ):
+def draw_square(num_sides):
     for i in range(4):
         turtle.left(90)
-        turtle.forward(num_sides) #draw square
+        turtle.forward(num_sides)
     
-
-def draw_triangle( number ):
+def draw_triangle(number):
     for i in range(4):
         turtle.left(45)
         turtle.forward(number)
         turtle.left(90)
         turtle.forward(number)
-        turtle.right(45) # draw triangles
+        turtle.right(45) 
 
-
-turtle.screensize() 
-turtle.window_width() 
-turtle.window_height()
-
-#turtle.exitonclick()
-
-
-num_sides = 2
-num_half_side = num_sides / 2
+num_sides = 10.5 # side length of square
+num_half_side = num_sides / 2 
 num_triangle_sides = (num_half_side **2 + num_half_side **2 )
-number = (num_triangle_sides**0.5)
+number = (num_triangle_sides**0.5) # hypotenuse of triangle
 print(number)
 
-import turtle
-turtle.speed(10)
-
-def shape ( num_sides, number ):
+def shape (num_sides, number):
     draw_square( num_sides )
     #for i in range(4):
     #    turtle.left(90)
     #    turtle.forward(num_sides) #draw square
     
-    draw_triangle( number )
+    draw_triangle(number)
     #for i in range(4):
     #    turtle.left(45)
     #    turtle.forward(number)
@@ -49,18 +38,12 @@ def shape ( num_sides, number ):
     turtle.left(45)
     turtle.forward(num_sides)
 
-
-for j in range(8):
+g=6
+for j in range(g):
     print(2**j)
-    shape( num_sides * 2**j, number * 2**j )
+    shape(num_sides * 2**j, number * 2**j)
+draw_square(num_sides*2**g)
 
-
-turtle.setposition(number/2,number/2) 
-turtle.setposition(-number*64, number*64)
-turtle.setposition(number/2,number/2)
-turtle.setposition(number*64, -number*64)
-turtle.setposition(number/2,number/2)
-turtle.setposition (-number*64, -number*64)
 
 
 
